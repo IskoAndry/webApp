eb049c5e232d909bc59f9fc08c7ca6757280a8e0
isakov <isakov4852@mail.ru>
2025-06-28 20:13:47 +0300

    5 commit

--- example.js
+++ example.js
@@ -1,31 +1,38 @@
 
 
 
-const getModal = document.querySelector('#modal')
+
 const getRow = document.querySelector('#row')
 const getModalBackdrop = document.querySelector('#backdrop')
+const getProgress = document.querySelector('#progress')
+const getModal = document.querySelector('#modal')
+const getForm = document.querySelector('#form')
 
 getRow.addEventListener('click', openCard)
 getModalBackdrop.addEventListener('click', closeModal)
+getModal.addEventListener('change', toggleTech)
+getForm.addEventListener('submit', createTech)
+
+const APP_TITLE = document.title 
 
 const technologies = [
   {
     title: "HTML",
     description: "HyperText Markup Language - стандартный язык разметки для создания веб-страниц",
     type: "html",
-    done: false
+    done: true,
   },
   {
     title: "CSS",
     description: "Cascading Style Sheets - язык стилей для описания внешнего вида HTML-документов",
     type: "css",
-    done: false
+    done: true
   },
   {
     title: "JavaScript",
     description: "Язык программирования для создания интерактивных веб-страниц",
     type: "js",
-    done: false
+    done: true
   },
   {
     title: "Git",
@@ -48,7 +55,7 @@ const technologies = [
   {
     title: "SASS/SCSS",
     description: "Препроцессор CSS, добавляющий переменные, вложенность, миксины и другие возможности",
-    type: "css",
+    type: "SASS/SCSS",
     done: false
   },
   {
@@ -60,48 +67,176 @@ const technologies = [
   {
     title: "Bootstrap",
     description: "Популярный CSS-фреймворк для создания адаптивных веб-сайтов",
-    type: "css",
+    type: "bootstrap",
     done: false
   },
   {
     title: "Redux",
     description: "Библиотека для управления состоянием JavaScript-приложений",
-    type: "js",
+    type: "redux",
     done: false
   },
   {
     title: "REST API",
     description: "Архитектурный стиль для создания веб-сервисов",
-    type: "web",
+    type: "restApi",
     done: false
   },
   {
     title: "Node.js",
     description: "Среда выполнения JavaScript на стороне сервера",
-    type: "js",
+    type: "Node",
     done: false
   }
 ];
 
-console.log(technologies)
+// console.log(technologies)
 
-function openCard(){
-    getModal.classList.add('open')
-    console.log('1111')
+function openCard(event){
+  const data = event.target.dataset
+  const tech = technologies.find(t => t.type === data.type )
+  if(!tech) return
+    
+  openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech){
+const checked = tech.done ? 'checked' : ''
+
+  return `
+    <h2>${tech.title}</h2>
+    <p>${tech.description}</p>
+	<hr>
+	<div>
+		<input type="checkbox" id="done" ${checked} data-type="${tech.type}">
+		<label for="done">Выучил</label>
+	</div>
+  `
+}
+
+function toggleTech(e){
+  const type = e.target.dataset.type
+  const tech = technologies.find(t => t.type === type )
+  tech.done = e.target.checked
+
+  init()
+}
+
+function openModal(html, title = APP_TITLE){
+  document.title = `${title} | ${APP_TITLE}`
+  getModal.innerHTML = html
+  const modal = document.querySelector('.modal')
+  getModal.classList.add('open')
 }
 
 function closeModal() {
+  document.title = APP_TITLE
   getModal.classList.remove('open') 
-  console.log('2222')     
+  // console.log('2222')     
 }
 
-let html = ''
-for(let i=0; i<technologies.length; i++){
-  const tech = technologies[i]
-  html +=`
-  <div class="card">
-    <h3>${tech.title}</h3>
-  </div>
-  `
+function init(){
+  renderCards()
+  renderProgress()
+  
 }
-getRow.innerHTML = html
+
+function renderProgress(){
+  const percent = computerProgrPercent()
+
+  let background
+  if(percent <=30){
+    background = 'red'
+  }else if((percent > 30 && percent < 70)){
+    background = 'orange'
+  }else{
+    background = 'green'
+  }
+  
+  getProgress.style.background = background
+  getProgress.style.width = percent + ' %'
+  getProgress.textContent = percent ? percent + '%' : '0%'
+}
+
+function computerProgrPercent(){
+
+  if(technologies.length === 0){
+    return 0
+  }
+
+  let doneCount = 0
+  for(let i = 0; i<technologies.length; i++){
+    if(technologies[i].done === true) doneCount++
+  }
+    return (100 * doneCount / technologies.length).toFixed(0)
+
+}
+
+function renderCards(){
+  if(technologies.length === 0){
+    getRow.innerHTML = '<p class="empty> Технологий пока нет. Добавте технологию </p>'
+  }else{
+//     let html = ''
+// for(let i=0; i<technologies.length; i++){
+//   const tech = technologies[i]
+//   html += toCard(tech)
+// }
+// getRow.innerHTML = html
+
+    getRow.innerHTML = technologies.map(toCard).join('')
+
+  }
+}
+
+function toCard(tech){
+  // let doneClass = ''
+
+  // if(tech.done){
+  //   doneClass = 'done'
+  // }else{
+  //   doneClass = ''
+  // }
+
+  doneClass = tech.done ? 'done' : ''
+
+  return `<div class="card ${doneClass}" data-type='${tech.type}'> <h3 data-type='${tech.type}'>${tech.title}</h3> </div>  `
+}
+
+function isInvalid(title, discription){
+  return !title.value || !discription.value
+}
+
+function createTech(e){
+  e.preventDefault()
+
+  // const title = e.target.title
+  // const discription = e.target.discription
+
+  const {title, discription} = e.target
+
+  if(isInvalid(title, discription)){  
+    if(!title.value) title.classList.add('invalid')
+    if(!discription.value) discription.classList.add('invalid')
+
+    setTimeout(()=>{
+      title.classList.remove('invalid')
+      discription.classList.remove('invalid')
+    }, 2000)
+
+    return 
+  }
+
+  const newTech = {
+    title: title.value,
+    discription: discription.value,
+    done: false,
+    type: title.value.toLowerCase()
+  }
+  technologies.push(newTech)
+  title.value = ''
+  discription.value = ''
+
+  init()
+}
+
+init()
